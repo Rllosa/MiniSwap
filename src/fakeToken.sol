083b92168c7812999c07acd7e00fa9125b6ccc7a
Romain Llosa <42243802+Rllosa@users.noreply.github.com>
2024-11-19 11:19:10 +0400

    ERC20 token contracts

---
+++ src/fakeToken.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: TBD
+pragma solidity 0.8.20;
+
+import "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+
+contract FakeToken is ERC20 {
+    uint8 decimal;
+
+    constructor(
+        string memory name_,
+        string memory symbol_,
+        address tokenReceiver,
+        uint8 _decimal,
+        uint256 initialValue
+    ) ERC20(name_, symbol_) {
+        decimal = _decimal;
+        _mint(tokenReceiver, initialValue);
+    }
+
+    function decimals() public view override returns (uint8) {
+        return decimal;
+    }
+
+    function mint(address account, uint256 amount) external {
+        _mint(account, amount);
+    }
+}
